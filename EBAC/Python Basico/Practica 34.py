f068ed0245781cd1383fd2a552187cb1e8df6c24
albhermo1979 <albhermo1979@pontificia.edu.mx>
2026-01-18 15:01:20 -0600

    Hice un cambio en el cual agregue 2 lineas de impresion al programa

--- EBAC/Python Basico/Practica 34.py
+++ EBAC/Python Basico/Practica 34.py
@@ -1,3 +1,5 @@
 #Instruccion para hacer input de valores o datos de usuario
 nombre = input('Como te llamas?')
 print("Tu nombre es: ", nombre)
+edad = input('Cual es tu edad?')
+print("Tu edad es: ", edad)
